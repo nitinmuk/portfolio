7d81a70aff3c8e1da3bdd0f83b5decfe833e87c6
Nitin Mukesh <nitinmukes@gmail.com>
2020-09-24 13:50:40 +0800

    Added Footer Component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer className="bottom">
+            <div className="footer-copyright text-center py-3">
+                &copy; 2020 Copyright:
+          <a href="https://github.com/nitinmuk/" target="_blank" rel="noopener noreferrer"> Nitin Mukesh</a>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
