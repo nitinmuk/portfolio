8fa391e32a8996ea3976dd03f3f5d14206902b6f
Nitin Mukesh <nitinmukes@gmail.com>
2020-09-27 16:30:24 +0800

    Added comment

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -5,6 +5,7 @@ import Button from "react-bootstrap/Button";
 const contactForm = () => {
     const handleSubmit = event => {
         event.preventDefault();
+        //need to complete its implementation
     }
     return (
         <Form>
