583d6c8d6de7a073991e67f911e15f75989f3ae9
Nitin Mukesh <nitinmukes@gmail.com>
2020-09-28 21:01:31 +0800

    Added more styling around navigation links in header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -26,21 +26,21 @@ const Header = () => {
                             window.location.pathname === "/" || window.location.pathname === "/about"
                                 ? "nav-link active"
                                 : "nav-link"
-                        }>About
+                        }><h6>About</h6>
                     </Link>
                     <Link to="/portfolio"
                         className={
                             window.location.pathname === "/portfolio"
                                 ? "nav-link active"
                                 : "nav-link"
-                        }>Portfolio
+                        }><h6>Portfolio</h6>
                     </Link>
                     <Link to="/contact"
                         className={
                             window.location.pathname === "/contact"
                                 ? "nav-link active"
                                 : "nav-link"
-                        }>Contact
+                        }><h6>Contact</h6>
                     </Link>
                 </Nav>
             </Navbar.Collapse>
